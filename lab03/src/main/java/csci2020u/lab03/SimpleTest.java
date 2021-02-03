4b9337a990ce157713343d00c7a7670d5bec2276
PaulKatigbak <paulgabriel.katigbak@ontariotechu.net>
2021-02-03 08:07:02 -0700

    Lab03 with Extra Challenge

---
+++ lab03/src/main/java/csci2020u/lab03/SimpleTest.java
@@ -0,0 +1,12 @@
+/**
+ * This is just a test file
+ * 
+ */
+
+public class SimpleTest{
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+    }
+}
